b3c3ab75d0254cc747f2ea3b6bce35dcb89c5ea8
KimA-Mik <ananim38@gmail.com>
2022-06-16 13:03:52 +0300

    first commit

---
+++ src/main/java/ru/jobster/holodok/HolodokApplication.java
@@ -0,0 +1,13 @@
+package ru.jobster.holodok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolodokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolodokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/jobster/holodok/HolodokApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.jobster.holodok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolodokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
